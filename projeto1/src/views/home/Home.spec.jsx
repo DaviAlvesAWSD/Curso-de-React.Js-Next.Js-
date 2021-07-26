9635bb8e88bf6810712c6d3dce7bf52e72b04b7e
Davi Alves <alvesd7050@gmail.com>
2021-07-26 20:22:14 -0300

    Atualização Testes e snapshots 26-07-2021-20h20"

---
+++ projeto1/src/views/home/Home.spec.jsx
@@ -0,0 +1,11 @@
+//import { render, screen } from '@testing-library/react';
+//import App from './App';
+
+describe('<Home />', () => {
+
+  it('is a dummy test', () => {
+    expect(1).toBe(1);
+  });
+});
+
+
