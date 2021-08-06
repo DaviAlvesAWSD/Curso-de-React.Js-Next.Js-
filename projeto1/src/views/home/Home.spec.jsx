1c0a76356d8e1dee8228a894bc9f1a6f61549d61
Davi Alves <alvesd7050@gmail.com>
2021-08-06 18:27:57 -0300

    Atualização 06-08-2021 18h27 seção 3 finalizada

--- projeto1/src/views/home/Home.spec.jsx
+++ projeto1/src/views/home/Home.spec.jsx
@@ -1,8 +1,99 @@
-//import { render, screen } from '@testing-library/react';
-//import App from './App';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Home } from '.';
+
+import { rest } from 'msw';
+import { setupServer, SetupServer } from 'msw/node';
+
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (req,res,ctx) => {
+    return res(ctx.json([
+      {
+        userId: 1,
+        id: 1,
+        title: 'title1',
+        body: 'body1',
+        url: 'img1.jpg'
+      },
+      {
+        userId: 1,
+        id: 1,
+        title: 'title2',
+        body: 'body2',
+        url: 'img2.jpg'
+      },
+      {
+        userId: 1,
+        id: 1,
+        title: 'title3',
+        body: 'body3',
+        url: 'img3.jpg'
+      },
+    ])
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
 
 describe('<Home />', () => {
-  it('is a dummy test', () => {
-    expect(1).toBe(1);
+  beforeAll(() => {
+    server.listen();
   });
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should render search, posts and load more', async () => {
+   render(<Home />);
+   const noMorePosts = screen.getByText('Search not found');
+
+   //expect.assertions(3);
+
+   await waitForElementToBeRemoved(noMorePosts);
+ 
+
+  
+   const search = screen.getByPlaceholderText(/type your search/i);
+   expect(search).toBeInTheDocument();
+
+   const images = screen.getAllByRole('img', {name: /title/i});
+   expect(images).toHaveLength(2);
+   
+   const button = screen.getByRole('button', {name: /Load more pages/i});
+   expect(button).toBeInTheDocument();
+  });
+
+  it('should seach  for posts', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Search not found');
+
+    //expect.assertions(10);
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3' })).not.toBeInTheDocument();
+
+  });
+  it('should load more posts', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Search not found');
+ 
+    await waitForElementToBeRemoved(noMorePosts);
+    
+    const button = screen.getByRole('button', {name: /Load more pages/i});
+
+    userEvent.click(button);
+    expect(screen.getByRole('heading', {name: 'title3'})).toBeInTheDocument();
+    expect(button).toBeDisabled();
+   });
+  
 });
