f30fdf094496ccee290361493e46de8a48d679c0
Davi Alves <alvesd7050@gmail.com>
2021-08-14 17:57:46 -0300

    Atualização-14-08-2021-17h57-seção-4-useContext

--- projeto2/src/App.js
+++ projeto2/src/App.js
@@ -1,8 +1,18 @@
-import P from 'prop-types';
-import { useEffect, useMemo, useState, useRef } from 'react';
 import './App.css';
+import { AppContext } from './contexts/AppContext';
+import { Div } from './components/DivComponent';
 //import React, { useState, useEffect, useCallback, useMemo } from 'react';
 
+const App = () => {
+  return (
+    <AppContext>
+      <Div />
+    </AppContext>
+  );
+};
+
+export default App;
+/* useRef
 const Post = ({ post, handleClick }) => {
   console.log('Post renderizou');
   return (
@@ -12,7 +22,6 @@ const Post = ({ post, handleClick }) => {
     </div>
   );
 };
-
 Post.propTypes = {
   post: P.shape({
     id: P.number,
@@ -21,52 +30,43 @@ Post.propTypes = {
   }),
   handleClick: P.func,
 };
-
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
   const input = useRef(null);
   const contador = useRef(0);
   console.log('Pai renderizou');
-
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((r) => r.json())
       .then((r) => setPosts(r));
   }, []);
-
   useEffect(() => {
     input.current.focus();
     console.log(input.current);
   }, [value]);
-
   useEffect(() => {
     contador.current++;
   });
-
   const handleClick = (value) => {
     setValue(value);
   };
-
   return (
     <div className="App">
       <h6>Renderizou: {contador.current}x</h6>
       <p>
         <input ref={input} type="search" value={value} onChange={(e) => setValue(e.target.value)} />
       </p>
-
       {useMemo(() => {
         {
           return posts.length > 0 && posts.map((post) => <Post key={post.id} post={post} handleClick={handleClick} />);
         }
       }, [posts])}
-
       {posts.length <= 0 && <p> Ainda não existem posts</p>}
     </div>
   );
 }
-
-export default App;
+*/
 
 /* useMemo
 const Post = ({ post }) => {
@@ -78,7 +78,6 @@ const Post = ({ post }) => {
     </div>
   );
 };
-
 Post.propTypes = {
   post: P.shape({
     id: P.number,
@@ -86,12 +85,10 @@ Post.propTypes = {
     body: P.string,
   }),
 };
-
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
   console.log('Pai renderizou');
-
   useEffect(() => {
     setTimeout(() => {
       fetch('https://jsonplaceholder.typicode.com/posts')
@@ -99,19 +96,16 @@ function App() {
         .then((r) => setPosts(r));
     }, 5000);
   }, []);
-
   return (
     <div className="App">
       <p>
         <input type="search" value={value} onChange={(e) => setValue(e.target.value)} />
       </p>
-
       {useMemo(() => {
         {
           return posts.length > 0 && posts.map((post) => <Post key={post.id} post={post} />);
         }
       }, [posts])}
-
       {posts.length <= 0 && <p> Ainda não existem posts</p>}
     </div>
   );
@@ -123,24 +117,18 @@ const Button = ({ incrementButton }) => {
   console.log('filho redenrizou');
   return <button onClick={() => incrementButton(10)}>+</button>;
 };
-
 Button.propTypes = {
   incrementButton: P.func,
 };
-
 function App() {
   const [counter, SetCounter] = useState(0);
-
   const incrementCounter = useCallback((num) => {
     SetCounter((c) => c + num);
   }, []);
-
   console.log('Pai redenrizou');
-
   const btn = useMemo(() => {
     return <Button incrementButton={incrementCounter} />;
   }, [incrementCounter]);
-
   return (
     <div className="App">
       <p>Um titulo qualquer</p>
@@ -149,6 +137,4 @@ function App() {
     </div>
   );
 }
-
-
 */
