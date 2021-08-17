50ab32ce7d3562111956b1d5dd9d4f4e45742d1a
Davi Alves <alvesd7050@gmail.com>
2021-08-17 17:27:22 -0300

    Atualização-17-08-2021-17h21 useReducer

--- projeto2/src/App.js
+++ projeto2/src/App.js
@@ -1,8 +1,64 @@
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/DivComponent';
+//import { AppContext } from './contexts/AppContext';
+//import { Div } from './components/DivComponent';
 //import React, { useState, useEffect, useCallback, useMemo } from 'react';
 
+export const globalState = {
+  title: 'O título do contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda com ', action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+    case 'inicio': {
+      console.log('Chamou inicio');
+      return { ...state, title: 'O título do contexto' };
+    }
+  }
+  console.log('Nenhuma action encontrada...');
+  return { ...state };
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+
+  return (
+    <div>
+      <h1>
+        {title} {counter}
+      </h1>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'muda',
+            payload: new Date().toLocaleString('pt-br'),
+          })
+        }
+      >
+        Click
+      </button>
+      <button onClick={() => dispatch({ type: 'inicio' })}>inicio</button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>inverter</button>
+      <button onClick={() => dispatch({ type: '' })}>inverter2</button>
+    </div>
+  );
+};
+
+export default App;
+
+/* useContext
 const App = () => {
   return (
     <AppContext>
@@ -11,11 +67,7 @@ const App = () => {
   );
 };
 
-export default App;
-
-
-
-
+*/
 
 /* useRef
 const Post = ({ post, handleClick }) => {
@@ -143,4 +195,3 @@ function App() {
   );
 }
 */
-
